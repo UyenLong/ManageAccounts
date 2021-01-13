96e3e1853f1cfe8a3839ba4f11ea75ced792acff
UYEN THI LONG NGUYEN/LGEVH VS FUNCTIONAL TECHNOLOGY 4(uyen.nguyen@lge.com) <uyen.nguyen@lge.com>
2021-01-13 18:07:13 +0700

    update class account, inherited by user and admin class

--- main.cpp
+++ main.cpp
@@ -18,23 +18,21 @@ enum class CharacterType
 
 class Account
 {
-private:
+protected:
     string username;
     string password;
+    bool isActive;
 
 public:
     Account()
     {
         this->username = this->password = "";
-    }
-    Account(string accountUsername, string accountPassword)
-    {
-        this->username = accountUsername;
-        this->password = accountPassword;
+        isActive = false;
     }
     ~Account()
     {
         this->username = this->password = "";
+        this->isActive = false;
     }
     void setAccountInfo()
     {
@@ -42,14 +40,15 @@ public:
         cin >> this->username;
         cout << "Enter password: " << endl;
         cin >> this->password;
+        this->isActive = true;
     }
     void printAccountInfo()
     {
-        cout << "Username: " << this->username << "\tPassword: " << this->password << endl;
+        cout << "Username: " << this->username << "\tPassword: " << this->password << "\t Is Active:" << this->isActive << endl;
     }
-    pair<string, string> getAccountInfo()
+    pair<string, pair<string, bool>> getAccountInfo()
     {
-        return make_pair(this->username, this->password);
+        return make_pair(this->username, make_pair(this->password, this->isActive));
     }
     void login()
     {
@@ -62,38 +61,67 @@ public:
     }
 };
 
-class AccountManager
+class User : public Account
+{
+public:
+    User()
+    {
+        this->username = this->password = "";
+        isActive = false;
+    }
+    User(string accountUsername, string accountPassword, bool activeStatus)
+    {
+        this->username = accountUsername;
+        this->password = accountPassword;
+        this->isActive = activeStatus;
+    }
+    ~User()
+    {
+        this->username = this->password = "";
+        this->isActive = false;
+    }
+    void deleteAccount()
+    {
+        this->isActive = false;
+    }
+};
+
+class Admin : public Account
 {
 private:
-    int totalAccount;
-    vector<Account> account;
+    int totalAccounts;
+    vector<User> managedAccounts;
 
 public:
-    AccountManager()
+    Admin()
     {
-        this->totalAccount = 0;
-        this->account = {};
+        this->username = this->password = "";
+        isActive = false;
+        this->totalAccounts = 0;
+        this->managedAccounts = {};
     }
-    ~AccountManager()
+    ~Admin()
     {
-        this->totalAccount = 0;
-        this->account = {};
+        this->username = this->password = "";
+        isActive = false;
+        this->totalAccounts = 0;
+        this->managedAccounts = {};
     }
-    void setListOfAccounts(map<string, string> listOfAccounts)
+    void setListOfAccounts(map<string, pair<string, bool>> listOfAccounts)
     {
-        this->totalAccount = listOfAccounts.size();
-        map<string, string>::iterator inputListAccount;
-        for (inputListAccount = listOfAccounts.begin(); inputListAccount != listOfAccounts.end(); ++inputListAccount)
+        this->totalAccounts = listOfAccounts.size();
+        map<string, pair<string, bool>>::iterator account;
+        for (account = listOfAccounts.begin(); account != listOfAccounts.end(); ++account)
         {
-            this->account.push_back(Account(inputListAccount->first, inputListAccount->second));
+            this->managedAccounts.push_back(User(account->first, account->second.first, account->second.second));
         }
     }
-    map<string, string> getListOfAccounts()
+    map<string, pair<string, bool>> getListOfAccounts()
     {
-        map<string, string> listOfAccounts;
-        for (size_t accountId = 0; accountId < this->totalAccount; ++accountId)
+        map<string, pair<string, bool>> listOfAccounts;
+        for (size_t accountId = 0; accountId < this->totalAccounts; ++accountId)
         {
-            listOfAccounts.insert(this->account[accountId].getAccountInfo());
+            listOfAccounts.insert(this->managedAccounts[accountId].getAccountInfo());
         }
         return listOfAccounts;
     }
@@ -101,62 +129,105 @@ public:
     {
         cout << "------------------------------------------------------------" << endl
              << "List of Accounts:" << endl;
-        for (size_t accountId = 0; accountId < this->totalAccount; ++accountId)
+        for (size_t account = 0; account < this->totalAccounts; ++account)
         {
-            this->account[accountId].printAccountInfo();
+            this->managedAccounts[account].printAccountInfo();
         }
         cout << "------------------------------------------------------------" << endl;
     }
-    void createNewAccount(Account newAccount)
+    void createNewAccount(User newAccount)
     {
-        this->totalAccount++;
+        this->totalAccounts++;
         cout << "Username: " << newAccount.getAccountInfo().first << "\t"
-             << "Password: " << newAccount.getAccountInfo().second << endl;
-        this->account.push_back(Account(newAccount.getAccountInfo().first, newAccount.getAccountInfo().second));
+             << "Password: " << newAccount.getAccountInfo().second.first << endl;
+        this->managedAccounts.push_back(User(newAccount.getAccountInfo().first, newAccount.getAccountInfo().second.first, newAccount.getAccountInfo().second.second));
         cout << "Create new account successful!" << endl;
     }
-    void deleteAnAccount(Account deleteAccount)
+    void deleteAnAccount(User deleteAccount)
     {
-        this->totalAccount--;
-        for (auto accountInList = this->account.begin(); accountInList != this->account.end();)
+        this->totalAccounts--;
+        for (auto accountInList = this->managedAccounts.begin(); accountInList != this->managedAccounts.end();)
         {
             if (accountInList->getAccountInfo() == deleteAccount.getAccountInfo())
             {
-                accountInList = this->account.erase(accountInList);
+                accountInList = this->managedAccounts.erase(accountInList);
                 break;
             }
         }
     }
+    void deleteListOfAccounts(vector<User> listOfDeleteAccounts)
+    {
+        this->totalAccounts -= listOfDeleteAccounts.size();
+        for (auto accountInTextFile = this->managedAccounts.begin(); accountInTextFile != this->managedAccounts.end();)
+        {
+            for (auto accountInDeleteList = listOfDeleteAccounts.begin(); accountInDeleteList != listOfDeleteAccounts.end();)
+            {
+                if (accountInTextFile == accountInDeleteList)
+                {
+                    accountInTextFile = this->managedAccounts.erase(accountInTextFile);
+                }
+            }
+        }
+    }
+    vector<User> getListOfInactiveAccounts()
+    {
+        vector<User> listOfInactiveAccounts;
+        vector<User>::iterator account;
+        for (account = this->managedAccounts.begin(); account != this->managedAccounts.end(); ++account)
+        {
+            if (account->getAccountInfo().second.second)
+            {
+                listOfInactiveAccounts.push_back(User(account->getAccountInfo().first, account->getAccountInfo().second.first, account->getAccountInfo().second.second));
+            }
+        }
+        return listOfInactiveAccounts;
+    }
 };
 
-map<string, string> getListOfAccountsFromTextFile(string fileName)
+bool convertStringToBool(string stringValue)
 {
-    map<string, string> listOfAccounts;
+    bool returnValue = true;
+    if (stringValue == "0")
+    {
+        returnValue = false;
+    }
+    return returnValue;
+}
+
+map<string, pair<string, bool>> getListOfAccountsFromTextFile(string fileName)
+{
+    map<string, pair<string, bool>> listOfAccounts;
     ifstream fileText;
     fileText.open(fileName);
     string accountInfo;
     while (getline(fileText, accountInfo))
     {
         size_t spaceCharacter = accountInfo.find(" ");
-        listOfAccounts[accountInfo.substr(0, spaceCharacter)] = accountInfo.substr(spaceCharacter + 1);
+        string username = accountInfo.substr(0, spaceCharacter);
+        accountInfo = accountInfo.substr(spaceCharacter + 1, accountInfo.size() - 1);
+        spaceCharacter = accountInfo.find(" ");
+        pair<string, bool> passwordAndStatus;
+        passwordAndStatus.first = accountInfo.substr(0, spaceCharacter);
+        passwordAndStatus.second = convertStringToBool(accountInfo.substr(spaceCharacter + 1));
+        listOfAccounts[username] = passwordAndStatus;
     }
     fileText.close();
     return listOfAccounts;
 }
 
-void updateListOfAccountsToTextFile(string fileName, map<string, string> newListOfAccounts)
+void updateListOfAccountsToTextFile(string fileName, map<string, pair<string, bool>> newListOfAccounts)
 {
     ofstream fileText;
     fileText.open(fileName);
-    map<string, string>::iterator account;
+    map<string, pair<string, bool>>::iterator account;
     for (account = newListOfAccounts.begin(); account != newListOfAccounts.end(); ++account)
     {
-        fileText << account->first << " " << account->second << endl;
+        fileText << account->first << " " << account->second.first << " " << account->second.second << endl;
     }
     fileText.close();
 }
 
-bool hasAccountExisted(string username, map<string, string> listOfAccounts)
+bool hasAccountExisted(string username, map<string, pair<string, bool>> listOfAccounts)
 {
     bool hasExisted = false;
     auto account = listOfAccounts.find(username);
@@ -209,28 +280,13 @@ bool isPasswordInCorrectForm(string password)
     return isStringHaveCharacterType(password, typeUpper) && isStringHaveCharacterType(password, typeLower) && isStringHaveCharacterType(password, typeSpecial) && isLengthOfStringGreaterThan(password, PASSWORD_LENGTH);
 }
 
-char getSelectedMenu()
-{
-    char selectedMenu;
-    cout << "Please enter according number listed below: " << endl
-         << "Login - Enter 1" << endl
-         << "Change password - Enter 2" << endl
-         << "Create new account - Enter 3" << endl
-         << "Delete an account - Enter 4" << endl
-         << "-------------------------------------" << endl
-         << "Press any key to exit" << endl
-         << "Your selection is: " << endl;
-    cin >> selectedMenu;
-    return selectedMenu;
-}
-
-bool isValidAccount(pair<string, string> checkAccount, map<string, string> listOfAccounts)
+bool isValidAccount(pair<string, pair<string, bool>> checkAccount, map<string, pair<string, bool>> listOfAccounts)
 {
     bool isValid = false;
-    map<string, string>::iterator account;
+    map<string, pair<string, bool>>::iterator account;
     for (account = listOfAccounts.begin(); account != listOfAccounts.end(); ++account)
     {
-        if (account->first == checkAccount.first && account->second == checkAccount.second)
+        if (account->first == checkAccount.first && account->second.first == checkAccount.second.first)
         {
             isValid = true;
             break;
@@ -239,15 +295,31 @@ bool isValidAccount(pair<string, string> checkAccount, map<string, string> listO
     return isValid;
 }
 
+char getSelectedMenu()
+{
+    char selectedMenu;
+    cout << "Please enter according number listed below: " << endl
+         << "Login - Enter 1" << endl
+         << "Change password - Enter 2" << endl
+         << "Create new account - Enter 3" << endl
+         << "Delete an account using admin account - Enter 4" << endl
+         << "Delete an account using user account - Enter 5" << endl
+         << "-------------------------------------" << endl
+         << "Press any key to exit" << endl
+         << "Your selection is: " << endl;
+    cin >> selectedMenu;
+    return selectedMenu;
+}
+
 int main()
 {
-    AccountManager accountManager;
-    Account account;
+    Admin accountManager;
+    User account;
     string newPassword;
     char selectedMenu = getSelectedMenu();
     account.setAccountInfo();
-    pair<string, string> accountInfo = account.getAccountInfo();
-    map<string, string> textFile = getListOfAccountsFromTextFile(ACCOUNT_FILE);
+    pair<string, pair<string, bool>> accountInfo = account.getAccountInfo();
+    map<string, pair<string, bool>> textFile = getListOfAccountsFromTextFile(ACCOUNT_FILE);
     accountManager.setListOfAccounts(textFile);
     cout << "List of accounts before changing..." << endl;
     accountManager.printListOfAccounts();
@@ -259,7 +331,9 @@ int main()
             account.login();
         }
         else
+        {
             cout << "Login fail..." << endl;
+        }
         break;
     case '2':
         cout << "Enter new password: " << endl;
@@ -267,23 +341,44 @@ int main()
         if (isValidAccount(account.getAccountInfo(), textFile) && isPasswordInCorrectForm(newPassword))
         {
             account.changePassword(newPassword);
+            accountManager.printListOfAccounts();
+            updateListOfAccountsToTextFile(ACCOUNT_FILE, accountManager.getListOfAccounts());
         }
         else
+        {
             cout << "Please enter valid account or new password in correct format!" << endl;
+        }
         break;
     case '3':
-        if (!hasAccountExisted(accountInfo.first, accountManager.getListOfAccounts()) && isPasswordInCorrectForm(accountInfo.second))
+        if (!hasAccountExisted(accountInfo.first, accountManager.getListOfAccounts()) && isPasswordInCorrectForm(accountInfo.second.first))
         {
             accountManager.createNewAccount(account);
             updateListOfAccountsToTextFile(ACCOUNT_FILE, accountManager.getListOfAccounts());
         }
         else
+        {
             cout << "Please enter another username or password" << endl;
+        }
         cout << "List of accounts after changing..." << endl;
         accountManager.printListOfAccounts();
         break;
     case '4':
         accountManager.deleteAnAccount(account);
+        updateListOfAccountsToTextFile(ACCOUNT_FILE, accountManager.getListOfAccounts());
+        cout << "List of accounts after changing..." << endl;
+        accountManager.printListOfAccounts();
+        break;
+    case '5':
+        account.deleteAccount();
+        bool isApprove;
+        cout << "As an Admin," << endl
+             << "Please enter 0 if approve the request." << endl
+             << "Please press any key if disapprove the request." << endl;
+        cin >> isApprove;
+        if (!isApprove)
+        {
+            accountManager.deleteAnAccount(account);
+        }
         updateListOfAccountsToTextFile(ACCOUNT_FILE, accountManager.getListOfAccounts());
         cout << "List of accounts after changing..." << endl;
         accountManager.printListOfAccounts();
